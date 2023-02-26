4f6ba23bf3fdcb967920d120f2818f3ba3a0bcc7
karthikyelisetti <62472452+karthikyelisetti@users.noreply.github.com>
2023-02-26 11:00:37 +0530

    updating student details code

--- student-details.js
+++ student-details.js
@@ -1,81 +1,99 @@
 class Node {
-    constructor(id, name, marks) {
-      this.id = id;
-      this.name = name;
-      this.marks = marks;
-      this.left = null;
-      this.right = null;
-      this.total = marks.reduce((sum, mark) => sum + mark, 0);
-      this.percentage = this.total / 5;
+  constructor(student_id, student_name, student_marks) {
+    this.student_id = student_id;
+    this.student_name = student_name;
+    this.student_marks = student_marks;
+    this.left = null;
+    this.right = null;
+    this.total = student_marks.forEach(student => {
+      let total_marks = 0;
+      total_marks += student.marks;
+    });
+    this.percentage = this.total / 5;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+
+  addStudent(student_id, student_name, student_marks) {
+    const node = new Node(student_id, student_name, student_marks);
+
+    if (this.root === null) {
+      this.root = node;
+    } else {
+      this.appendStudent(this.root, node);
     }
   }
-  
-  class BinarySearchTree {
-    constructor() {
-      this.root = null;
-    }
-  
-    insert(id, name, marks) {
-      const node = new Node(id, name, marks);
-  
-      if (this.root === null) {
-        this.root = node;
+
+  appendStudent(currentNode, newNode) {
+    if (newNode.percentage < currentNode.percentage) {
+      if (currentNode.left === null) {
+        currentNode.left = newNode;
       } else {
-        this.insertNode(this.root, node);
+        this.appendStudent(currentNode.left, newNode);
       }
-    }
-  
-    insertNode(currentNode, newNode) {
-      if (newNode.percentage < currentNode.percentage) {
-        if (currentNode.left === null) {
-          currentNode.left = newNode;
-        } else {
-          this.insertNode(currentNode.left, newNode);
-        }
+    } else {
+      if (currentNode.right === null) {
+        currentNode.right = newNode;
       } else {
-        if (currentNode.right === null) {
-          currentNode.right = newNode;
-        } else {
-          this.insertNode(currentNode.right, newNode);
-        }
-      }
-    }
-  
-    find(name) {
-      return this.findNode(this.root, name);
-    }
-  
-    findNode(currentNode, name) {
-      if (currentNode === null) {
-        return null;
-      }
-  
-      if (currentNode.name === name) {
-        return currentNode.marks;
-      }
-  
-      if (name < currentNode.name) {
-        return this.findNode(currentNode.left, name);
-      } else {
-        return this.findNode(currentNode.right, name);
+        this.appendStudent(currentNode.right, newNode);
       }
     }
   }
-  
-  // example usage
-  const bst = new BinarySearchTree();
-  
-  bst.insert(1, "Chandan", [80, 90, 85, 75, 95]);
-  bst.insert(2, "Bipul", [70, 85, 80, 90, 95]);
-  bst.insert(3, "Adi", [60, 75, 70, 80, 85]);
-  
-//   const studentName = prompt("Enter student name:");
-  let studentName = "Chandan"
-  const studentMarks = bst.find(studentName);
-  
-  if (studentMarks) {
-    console.log(`Student marks for ${studentName}: ${studentMarks.join(", ")}`);
-  } else {
-    console.log(`Student ${studentName} not found`);
+
+  search(sname) {
+    return this.searchStudent(this.root, sname);
   }
-  
+
+  searchStudent(currentNode, sname) {
+    if (currentNode === null) {
+      return null;
+    }
+
+    if (currentNode.student_name === sname) {
+      return currentNode.student_marks;
+    }
+
+    if (sname < currentNode.student_name) {
+      return this.searchStudent(currentNode.left, sname);
+    } else {
+      return this.searchStudent(currentNode.right, sname);
+    }
+  }
+
+  printDetails(studentName, studentMarks) {
+    let counter = 0;
+    let total = 0;
+    if (studentMarks) {
+      studentMarks.forEach(student => {
+        if (counter == 0) { console.log(`Student Name: ${studentName}`); }
+        counter += 1;
+        total += student.marks;
+        console.log(`${student.subject}: ${student.marks}`);
+      });
+      total = (total / studentMarks.length);
+      console.log("*********************");
+      console.log(`Grade: ${total}%`);
+      console.log("*********************");
+    }    
+  }
+}
+
+//creating instance of a class BinarySearchTree
+const bst = new BinarySearchTree();
+
+bst.addStudent(855, "James", [{subject: "Maths", marks: 75}, {subject: "Physics", marks: 80}, {subject: "Chemistry", marks: 75}, {subject: "Social", marks: 70}, {subject: "Computer Science", marks: 90}]);
+bst.addStudent(856, "Hari", [{subject: "Maths", marks: 90}, {subject: "Physics", marks: 70}, {subject: "Chemistry", marks: 75}, {subject: "Social", marks: 65}, {subject: "Computer Science", marks: 80}]);
+bst.addStudent(854, "Ravi", [{subject: "Maths", marks: 85}, {subject: "Physics", marks: 90}, {subject: "Chemistry", marks: 85}, {subject: "Social", marks: 60}, {subject: "Computer Science", marks: 75}]);
+
+console.log("==========================");
+console.log("Searching Student Details");
+console.log("==========================");
+
+let studentName = "Ravi"
+const studentMarks = bst.search(studentName);
+
+bst.printDetails(studentName, studentMarks);
