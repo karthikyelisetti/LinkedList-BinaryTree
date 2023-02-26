180c29dff336be0fea1faba9362ad6d8870913dd
karthikyelisetti <62472452+karthikyelisetti@users.noreply.github.com>
2023-02-27 00:10:44 +0530

    updated customer details js file

--- customer-details.js
+++ customer-details.js
@@ -1,99 +1,166 @@
 class Customer {
-    constructor(id, name, purchaseDate, billAmount) {
-      this.id = id;
-      this.name = name;
-      this.purchaseDate = purchaseDate;
-      this.billAmount = billAmount;
-      this.next = null;
-    }
+  constructor(customer_id, customer_name, purchase_date, amount) {
+    this.customer_id = customer_id;
+    this.customer_name = customer_name;
+    this.purchase_date = purchase_date;
+    this.amount = amount;
+    this.next = null;
   }
-  
-  class CustomerList {
-    constructor() {
-      this.head = null;
+}
+
+class CustomerDetails {
+  constructor() {
+    this.head = null;
+  }
+
+  addCustomerDetails(customer_id, customer_name, purchase_date, amount) {
+    let newCustomer = new Customer(
+      customer_id,
+      customer_name,
+      purchase_date,
+      amount
+    );
+    if (!this.head) {
+      this.head = newCustomer;
+      return;
     }
-  
-    addCustomer(id, name, purchaseDate, billAmount) {
-      let newCustomer = new Customer(id, name, purchaseDate, billAmount);
-      if (!this.head) {
-        this.head = newCustomer;
-        return;
-      }
+    let current = this.head;
+    while (current.next) {
+      current = current.next;
+    }
+    current.next = newCustomer;
+  }
+
+  sortAmount() {
+    let swapped;
+    do {
+      swapped = false;
       let current = this.head;
-      while (current.next) {
-        current = current.next;
-      }
-      current.next = newCustomer;
-    }
-  
-    sortByBillAmount() {
-      let swapped;
-      do {
-        swapped = false;
-        let current = this.head;
-        while (current && current.next) {
-          if (current.billAmount > current.next.billAmount) {
-            let temp = current.billAmount;
-            current.billAmount = current.next.billAmount;
-            current.next.billAmount = temp;
-            swapped = true;
-          }
-          current = current.next;
-        }
-      } while (swapped);
-    }
-  
-    getTotalPurchaseAmount(year) {
-      let total = 0;
-      let current = this.head;
-      while (current) {
-        let dateParts = current.purchaseDate.split("/");
-        if (dateParts[2] == year) {
-          total += current.billAmount;
+      while (current && current.next) {
+        if (current.amount > current.next.amount) {
+          let temp = current.amount;
+          current.amount = current.next.amount;
+          current.next.amount = temp;
+          swapped = true;
         }
         current = current.next;
       }
-      return total;
-    }
-  
-    getCustomerByName(name) {
-      let current = this.head;
-      while (current) {
-        if (current.name == name) {
-          return current;
-        }
-        current = current.next;
+    } while (swapped);
+  }
+
+  custPurchaseAmount(year) {
+    let total = 0;
+    let current = this.head;
+    while (current) {
+      let splitDate = current.purchase_date.split("/");
+      if (splitDate[2] == Number(year)) {
+        total += parseInt(current.amount);
       }
-      return null;
+      current = current.next;
     }
+    return total;
   }
-  
-  let customerList = new CustomerList();
-  customerList.addCustomer(1, "Chandan", "15/01/22", 100);
-  customerList.addCustomer(2, "Bipul", "20/02/22", 200);
-  customerList.addCustomer(3, "Adi", "10/03/22", 300);
-  
-  
-  
-  customerList.sortByBillAmount();
-  let current = customerList.head;
-  console.log("All customer data sorted by bill amount:");
-  while (current) {
-    console.log(current.id, current.name, current.purchaseDate, current.billAmount);
-    current = current.next;
+
+  findCustomer(customer_name) {
+    let current = this.head;
+    while (current) {
+      if (current.customer_name == customer_name) {
+        return current;
+      }
+      current = current.next;
+    }
+    return null;
   }
-  
-  
-  let year = "22";
-  let total = customerList.getTotalPurchaseAmount(year);
-  console.log(`Total purchase amount for ${year}: ${total}`);
-  
-  let name = "Purva";
-  let customer = customerList.getCustomerByName(name);
-  if (customer) {
-    console.log(`Details of customer ${name}:`);
-    console.log(customer.id, customer.name, customer.purchaseDate, customer.billAmount);
-  } else {
-    console.log(`Customer ${name} not found`);
-  }
-  
+}
+
+let customerList = new CustomerDetails();
+
+customerList.addCustomerDetails("C246123", "Jeremy", "15/01/23", "3000");
+customerList.addCustomerDetails("C325435", "Travis", "20/02/23", "2400");
+customerList.addCustomerDetails("C436879", "Archie", "10/12/22", "2700");
+customerList.addCustomerDetails("C246639", "Fabien", "15/01/23", "3500");
+customerList.addCustomerDetails("C325525", "Walter", "20/02/23", "2100");
+customerList.addCustomerDetails("C436630", "Archie", "10/12/22", "4500");
+
+customerList.sortAmount();
+let current = customerList.head;
+console.log(
+  "-----------------------------------------------------------------------"
+);
+console.log(
+  "| Customer data in sorted order based on bill amount                  |"
+);
+console.log(
+  "-----------------------------------------------------------------------"
+);
+console.log(
+  "| Customer ID | Customer Name | Purchase Date(dd/mm/yy) | Bill Amount |"
+);
+console.log(
+  "-----------------------------------------------------------------------"
+);
+while (current) {
+  console.log(
+    "| " +
+      current.customer_id +
+      "     | " +
+      current.customer_name +
+      "        | " +
+      current.purchase_date +
+      "                | " +
+      current.amount +
+      "        |"
+  );
+  current = current.next;
+}
+
+let year = "22";
+console.log("   ");
+console.log("   ");
+console.log(
+  "-----------------------------------------------------------------------"
+);
+console.log(
+  "| Total purchase amount for a specific year                           |"
+);
+console.log(
+  "-----------------------------------------------------------------------"
+);
+let total = customerList.custPurchaseAmount(year);
+console.log(`Total purchase amount for the year'${year}: ${total}`);
+
+console.log("   ");
+console.log("   ");
+console.log(
+  "-----------------------------------------------------------------------"
+);
+console.log(
+  "| Details of a specific customer based on name                        |"
+);
+console.log(
+  "-----------------------------------------------------------------------"
+);
+console.log(
+  "| Customer ID | Customer Name | Purchase Date(dd/mm/yy) | Bill Amount |"
+);
+console.log(
+  "-----------------------------------------------------------------------"
+);
+let customer_name = "Archie";
+let customer = customerList.findCustomer(customer_name);
+customer
+  ? console.log(
+      "| " +
+        customer.customer_id +
+        "     | " +
+        customer.customer_name +
+        "        | " +
+        customer.purchase_date +
+        "                | " +
+        customer.amount +
+        "        |"
+    )
+  : console.log(`Customer ${customer_name} not found`);
+console.log(
+  "-----------------------------------------------------------------------"
+);
